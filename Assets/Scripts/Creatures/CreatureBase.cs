22ece2be5f28491bfef7375efd5849e74e342a9d
Benjamin Ledoux <bledoux2002@gmail.com>
2023-03-22 20:29:24 -0400

    creature class, expanding test area
    added creatures and class with stats, and expanded start/test area

---
+++ Assets/Scripts/Creatures/CreatureBase.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Creature", menuName = "Creature/Create new creature")]
+
+public class CreatureBase : ScriptableObject
+{
+    [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] CreatureType type1;
+    [SerializeField] CreatureType type2;
+
+    // Base Stats
+    [SerializeField] int maxHp;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+
+    public string Name {
+        get { return name; }
+    }
+
+    public string Description {
+        get { return description; }
+    }
+
+    public Sprite FrontSprite {
+        get { return frontSprite; }
+    }
+
+    public Sprite BackSprite {
+        get { return backSprite; }
+    }
+
+    public CreatureType Type1 {
+        get { return type1; }
+    }
+
+    public CreatureType Type2 {
+        get { return type2; }
+    }
+
+    public int MaxHp {
+        get { return maxHp; }
+    }
+
+    public int Attack {
+        get { return attack; }
+    }
+
+    public int Defense {
+        get { return defense; }
+    }
+
+    public int SpAttack {
+        get { return spAttack; }
+    }
+
+    public int SpDefense {
+        get { return spDefense; }
+    }
+
+    public int Speed {
+        get { return speed; }
+    }
+}
+
+public enum CreatureType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon
+}
